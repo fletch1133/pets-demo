2d9433e06f881a4c8d90979f4a585b0b8eb31f92
Anthony Fletcher <afletcher2433.com>
2023-03-01 16:45:41 -0500

    first commit

--- server/server.js
+++ server/server.js
@@ -1,6 +1,14 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
+const path = require('path')
+app.use(express.json())
+app.use(cors())
+
+app.use(express.static(`public`))
+
+app.get('/', (req,res) => {
+    res.status(200).sendFile(path.join(__dirname, '../public/index.html'))
+})
 
 app.listen(4000, () => console.log(`server running on 4000`))
-
-app.use(express.static(`${__dirname}/public`))
